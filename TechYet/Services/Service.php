e775b1cea301b3ca6f1bab5c26c7137238db16c1
Ali Hamze <ali@alihamze.com>
2017-12-07 11:21:59 -0500

    Created base service class

---
+++ TechYet/Services/Service.php
@@ -0,0 +1,34 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: alihamze
+	 * Date: 12/7/17
+	 * Time: 11:12 AM
+	 */
+	
+	namespace TechYet\Services;
+	
+	
+	use TechYet\TechYet;
+	
+	abstract class Service {
+		/**
+		 * @var TechYet
+		 */
+		private $_techYet;
+		
+		/**
+		 * Initializes service
+		 * @param TechYet $techYet
+		 */
+		public function __construct(TechYet $techYet) {
+			$this->_techYet = $techYet;
+		}
+		
+		/**
+		 * @return TechYet
+		 */
+		public function getTechYet() {
+			return $this->_techYet;
+		}
+	}

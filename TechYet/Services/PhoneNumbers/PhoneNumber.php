b3d5dfc2980010ee0bb3391709c12129488ab50b
Ali Hamze <ali@alihamze.com>
2018-07-05 17:40:09 -0400

    Added incoming calls method

--- TechYet/Services/PhoneNumbers/PhoneNumber.php
+++ TechYet/Services/PhoneNumbers/PhoneNumber.php
@@ -18,11 +18,13 @@
 	 * @property string $id
 	 * @property string $phoneNumber
 	 * @property array $capabilities
+	 * @property string $incoming_calls_method
 	 */
 	class PhoneNumber extends ListItem {
 		protected $id;
 		protected $phoneNumber;
 		protected $capabilities;
+		protected $incoming_calls_method;
 		
 		/**
 		 * Constructs from the API data
@@ -33,6 +35,7 @@
 			$this->id = $data['id'];
 			$this->phoneNumber = $data['phone_number'];
 			$this->capabilities = $data['capabilities'];
+			$this->incoming_calls_method = $data['incoming_calls'];
 			
 			foreach ($this->capabilities as $key => $value) {
 				$this->capabilities[$key] = boolval($value);
